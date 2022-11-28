bad1112d63e838db31f535f6715d06524572d90c
SiQunChen <92172174+SiQunChen@users.noreply.github.com>
2022-11-29 01:06:36 +0800

    Update cookie.h

--- HW01/cookie.h
+++ HW01/cookie.h
@@ -1,63 +1,91 @@
-#include "../src/cookie.h"
+#ifndef COOKIE_H
+#define COOKIE_H
+using namespace std;
 
-TEST(cookie, Array1)
-{
-    int cookie = 4;
-    int array[4] = {1,3,5,6};
-    int output = max_dv(cookie, array);
-    ASSERT_EQ(output, 9);
+int check_x(bool** grid){
+    /* Return the x-coordinate of the lastest of cookie. */
+     for(int x=0;x<10;x++)
+     {
+        for(int y=9;y>=0;y--)
+        {
+            if(grid[x][y]==true)return x;//printf("%d%d",x,y);
+        }
+     }
+     return 0;
 }
-TEST(cookie, Array2)
-{
-    int cookie = 2;
-    int array[2] = {6,9};
-    int output = max_dv(cookie, array);
-    ASSERT_EQ(output, 9);
+
+int check_y(bool** grid){
+    /* Return the y-coordinate of the lastest of cookie. */
+    for(int x=0;x<10;x++)
+     {
+        for(int y=9;y>=0;y--)
+        {
+            if(grid[x][y]==true)return y;
+        }
+     }
+     return 0;
 }
-TEST(cookie, Array3)
-{
-    int cookie = 3;
-    int array[3] = {3,6,9};
-    int output = max_dv(cookie, array);
-    ASSERT_EQ(output, 12);
+
+int print_grid(bool** grid){
+    /* Print the grid you input. */
+    /* Implement this function if necessary, it can help you to check your grid is correct or not. */
+    for(int i=0;i<10;i++)
+    {
+        for(int j=0;j<10;j++)
+        {
+            printf("%d ",grid[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
 }
-TEST(cookie, Array4)
-{
-    int cookie = 5;
-    int array[5] = {1,3,5,6,7};
-    int output = max_dv(cookie, array);
-    ASSERT_EQ(output, 13);
+
+void placeTop(bool** grid){
+    /* Get the coordinate of the cookie which place lastest from grid. */
+    /* You can implement check_x(), check_y() above to get the x, y of the lastest cookie you place. */
+    int x = check_x(grid);
+    int y = check_y(grid);
+    x-=1;
+    /* Check when you place cookie, it will out of bound or not. */
+    if(x<0){ throw string("out of range");}
+    /* ... */
+
+    /* Place cookie from the top of current cookie. */
+    grid[x][y]=true;
 }
-TEST(cookie, Array5)
-{
-    int cookie = 4;
-    int array[4] = {1,-3,5,6};
-    int output = max_dv(cookie, array);
-    ASSERT_EQ(output, -1);
+
+void placeRight(bool** grid){
+    /* Get the coordinate of the cookie which place lastest from grid. */
+    int x = check_x(grid);
+    int y = check_y(grid);
+    y+=1;
+    /* Check when you place cookie, it will out of bound or not. */
+    if(y>9){ throw string("out of range");}
+
+    /* Place cookie from the right of current cookie. */
+    grid[x][y]=true;
 }
-TEST(cookie, Array6)
-{
-    int cookie = 2;
-    int array[2] = {-5,6};
-    int output = max_dv(cookie, array);
-    ASSERT_EQ(output, -1);
+
+int calcRow(bool** grid, int row){
+    /* Calculate the count of cookie on the specific row */
+    if(row>9 || row<0){return -1;}
+    int total=0;
+    for(int x=0;x<10;x++)
+    {
+        if(grid[row][x]==true){total+=1;}
+    }
+    return total;
 }
-TEST(cookie, Array7)
-{
-    int cookie = -4;
-    int array[4] = {1,3,5,6};
-    int output = max_dv(cookie, array);
-    ASSERT_EQ(output, -1);
+
+int calcColumn(bool** grid, int column){
+    /* Calculate the count of cookie on the specific column */
+    if(column>9 || column<0){return -1;}
+    int total=0;
+    for(int y=0;y<10;y++)
+    {
+        if(grid[y][column]==true){total+=1;}
+    }
+    return total;
 }
-TEST(cookie, Array8)
-{
-    int cookie = -2;
-    int array[2] = {5,7};
-    int output = max_dv(cookie, array);
-    ASSERT_EQ(output, -1);
-}
-// A single test case has the following form:
-//
-// TEST(TestSuiteName, TestName) {
-//   ... test body ...
-// }
+
+#endif
