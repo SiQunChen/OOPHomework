b37b5fcb8533ee63cc8531e673f7b1f5e14de760
SiQunChen <92172174+SiQunChen@users.noreply.github.com>
2022-12-05 02:35:37 +0800

    Create alcohol.h

---
+++ HW05/alcohol.h
@@ -0,0 +1,52 @@
+#ifndef ALCOHOL_H
+#define ALCOHOL_H
+#include <bits/stdc++.h>
+
+class Alcohol{
+    protected:
+        std::string _name = "";
+        int _price = -1;
+        double _content = -1;
+        
+    public:
+        void init(std::string name, int price, double content){
+            if(name.length() == 0){
+                throw std::invalid_argument("invalid_argument");
+            }
+            else if(price < 0){
+                throw std::invalid_argument("invalid_argument");
+            }
+            else if(content < 0 || content > 1){
+                throw std::invalid_argument("invalid_argument");
+            }
+            else{
+                _name = name;
+                _price = price;
+                _content = content;
+            }
+        }
+
+        Alcohol() = default;
+
+        Alcohol(std::string name, int price, double content){
+            init(name, price, content);
+	    }
+
+        virtual ~Alcohol(){}
+
+        virtual void dilute() = 0;
+
+        double get_alcohol_content(){
+            return _content;
+        }
+
+        int get_price(){
+            return _price;
+        }
+
+        std::string get_name(){
+            return _name;
+        }
+};
+
+#endif
